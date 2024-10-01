f3223208f9b4dd751597be9ed64aed975465d7b2
Bheemm1992 <bheemhepte@gmail.com>
2024-10-01 15:06:42 +0530

    Update ProductController.java

--- E-commerce-web-Application-main/SpringEcom/controller/ProductController.java
+++ E-commerce-web-Application-main/SpringEcom/controller/ProductController.java
@@ -84,13 +84,6 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("products/search")
-    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
-
-        List<Product> products=productService.searchProducts(keyword);
-        System.out.println("Searching with : " + keyword);
-        return new ResponseEntity<>(products,HttpStatus.OK);
-
-    }
+  
 
 }
